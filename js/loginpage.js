85da8e55807e9340091269843a86eae2d5cf5dc3
Kunalkumarfswd10722 <119043066+Kunalkumarfswd10722@users.noreply.github.com>
2023-10-04 12:03:51 +0530

    update

--- js/loginpage.js
+++
@@ -1,9 +0,0 @@
-var x = document.getElementById("login");
-var y = document.getElementById("register");
-var z = document.getElementById("btn");
-
-function register(){
-    x.style.left = "-400px";
-    y.style.left = "50px";
-    z.style.left = "110px"
-}
